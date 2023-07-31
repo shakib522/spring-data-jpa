bf67922f2ab25da714ab3b397208a5ff29ad5778
shakib522 <shakibhasann522@gmail.com>
2023-07-31 16:49:23 +0600

    paging and sorting implemented

--- src/main/java/com/shakib/dataJpa/repository/CourseRepository.java
+++ src/main/java/com/shakib/dataJpa/repository/CourseRepository.java
@@ -1,7 +1,14 @@
 package com.shakib.dataJpa.repository;
 
 import com.shakib.dataJpa.entity.Course;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CourseRepository extends JpaRepository<Course,Long> {
+public interface CourseRepository extends JpaRepository<Course, Long> {
+
+    Page<Course> findByTitleContaining(String title,
+                                       Pageable pageRequest);
+
 }
